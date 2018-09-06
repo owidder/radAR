1360ef3e88dadc2b134366999a47920140f9acf0
Oliver Widder <oliver.widder@gmail.com>
2018-09-06 18:28:50 +0200

    title-cube: back and front

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {initSlides} from './slides/slidesIndex';
 import {demoSlides} from "./slides/demoSlides/demoSlides";
 import * as dbar from "./slides/dbar/dbar";
 import * as slowSnakes from "./slides/slowSnakes/slowSnakes";
+import * as codeTalks from "./slides/codeTalks/codeTalks";
 
 import {appendStyles} from './util/loadStyles';
 
@@ -20,10 +21,19 @@ window.simplAR = simplAR;
 const BODY_TYPE_RADAR = "radar";
 const BODY_TYPE_CUBE = "cube";
 const BODY_TYPE_SLIDE_DEMO = "slideDemo";
+const BODY_TYPE_CODE_TALKS = "ct";
 const BODY_TYPE_SLIDE_DBAR = "dbar";
 const BODY_TYPE_SLIDE_SLOW_SNAKES = "slsn";
 
-const bodyType = query.firstParamSet([BODY_TYPE_SLIDE_DEMO, BODY_TYPE_CUBE, BODY_TYPE_RADAR, BODY_TYPE_SLIDE_DBAR, BODY_TYPE_SLIDE_SLOW_SNAKES]);
+const bodyType = query.firstParamSet([
+    BODY_TYPE_SLIDE_DEMO,
+    BODY_TYPE_CUBE,
+    BODY_TYPE_RADAR,
+    BODY_TYPE_SLIDE_DBAR,
+    BODY_TYPE_SLIDE_SLOW_SNAKES,
+    BODY_TYPE_CODE_TALKS,
+]);
+
 const paramValue = query.paramValue(bodyType);
 
 switch (bodyType) {
@@ -50,6 +60,11 @@ switch (bodyType) {
     case BODY_TYPE_SLIDE_SLOW_SNAKES:
         slidarGlobal.slidesFolder = "slides/slowSnakes/html/";
         initSlides("#container", slowSnakes.init, paramValue);
+        break;
+
+    case BODY_TYPE_CODE_TALKS:
+        slidarGlobal.slidesFolder = "slides/codeTalks/html/";
+        initSlides("#container", codeTalks.init, paramValue);
         break;
 
     default:
