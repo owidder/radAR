98e541f45b03a1eff5b2437980ac3e7f66cc1201
Oliver Widder <oliver.widder@gmail.com>
2018-09-28 16:19:53 +0200

    start-transaction

--- src/slides/codeTalks/codeTalks.js
+++ src/slides/codeTalks/codeTalks.js
@@ -38,6 +38,7 @@ export const init = async (rootSelector, selectedFilename) => {
         slidesUtil.createSlide(createFct, "drizzle", selectedFilename),
         slidesUtil.createSlide(createFct, "react-component", selectedFilename),
         slidesUtil.createSlide(createFct, "init-drizzle", selectedFilename),
+        slidesUtil.createSlide(createFct, "start-transaction", selectedFilename),
         slidesUtil.createSlide(createFct, "where-to-deploy", selectedFilename),
         slidesUtil.createSlide(createFct, "no-option", selectedFilename),
         slidesUtil.createSlide(createFct, "ropsten", selectedFilename),
